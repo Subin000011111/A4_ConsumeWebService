274fe4b0543aa80911258d42395dc8fff84e0e2f
Subin000011111 <subinsamuel104@gmail.com>
2025-04-14 16:45:21 -0400

    Implement Edit Book form with pre-filled data and update logic

--- src/app/components/edit-book/edit-book.component.ts
+++ src/app/components/edit-book/edit-book.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../models/book';
 
 @Component({
   selector: 'app-edit-book',
-  imports: [],
   templateUrl: './edit-book.component.html',
-  styleUrl: './edit-book.component.css'
+  styleUrls: ['./edit-book.component.css']
 })
-export class EditBookComponent {
+export class EditBookComponent implements OnInit {
+  book: Book = {
+    title: '',
+    authorName: '',
+    price: 0,
+    quantity: 0
+  };
 
+  constructor(
+    private route: ActivatedRoute,
+    private bookService: BookService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.bookService.getBookById(id).subscribe((data: Book) => {
+      this.book = data;
+    });
+  }
+
+  onSubmit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.bookService.updateBook(id, this.book).subscribe(() => {
+      alert('Book updated successfully!');
+      this.router.navigate(['/books']);
+    });
+  }
 }
