942c38c92a1e4c7bef26952a57818a6006978895
Subin000011111 <subinsamuel104@gmail.com>
2025-04-14 16:43:10 -0400

    Implement Add Book form and functionality

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../models/book';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-book',
-  imports: [],
   templateUrl: './add-book.component.html',
-  styleUrl: './add-book.component.css'
+  styleUrls: ['./add-book.component.css']
 })
 export class AddBookComponent {
+  book: Book = {
+    title: '',
+    authorName: '',
+    price: 0,
+    quantity: 0
+  };
 
+  constructor(private bookService: BookService, private router: Router) {}
+
+  onSubmit(): void {
+    this.bookService.createBook(this.book).subscribe(() => {
+      alert('Book added successfully!');
+      this.router.navigate(['/books']);
+    });
+  }
 }
