4051db2f3c85dafbe7b2f3777cfba1d122bc2c24
Subin000011111 <subinsamuel104@gmail.com>
2025-04-14 16:48:53 -0400

    Add Book Details component to display single book info

--- src/app/components/book-details/book-details.component.ts
+++ src/app/components/book-details/book-details.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookService } from '../../services/book.service';
+import { Book } from '../../models/book';
 
 @Component({
   selector: 'app-book-details',
-  imports: [],
   templateUrl: './book-details.component.html',
-  styleUrl: './book-details.component.css'
+  styleUrls: ['./book-details.component.css']
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit {
+  book: Book | null = null;
 
+  constructor(private route: ActivatedRoute, private bookService: BookService) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.bookService.getBookById(id).subscribe((data: Book) => {
+      this.book = data;
+    });
+  }
 }
