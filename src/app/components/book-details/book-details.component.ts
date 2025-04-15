f4db9faa7fab52c1806db2a07a5ad67bc7983572
Subin000011111 <subinsamuel104@gmail.com>
2025-04-14 23:39:53 -0400

    Implemented BookDetailsComponent to view details of a single book

--- src/app/components/book-details/book-details.component.ts
+++ src/app/components/book-details/book-details.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BookService } from '../../services/book.service';
 import { Book } from '../../models/book';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-details',
+  standalone: true,
   templateUrl: './book-details.component.html',
   styleUrls: ['./book-details.component.css'],
-  standalone: true,
   imports: [CommonModule]
 })
 export class BookDetailsComponent implements OnInit {
   book: Book | null = null;
 
-  constructor(private route: ActivatedRoute, private bookService: BookService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private bookService: BookService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.bookService.getBookById(id).subscribe((data: Book) => {
+    this.bookService.getBook(id).subscribe(data => {
       this.book = data;
     });
+  }
+
+  goBack(): void {
+    this.router.navigate(['/books']);
   }
 }
