70c083f5a5ce5371d6434c1496ff855a6892c847
Subin000011111 <subinsamuel104@gmail.com>
2025-04-15 00:20:51 -0400

    Created HomeComponent with 4 route links and set it as the default homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
 import { EditBookComponent } from './components/edit-book/edit-book.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },             
   { path: 'books', component: BookListComponent },
   { path: 'add', component: AddBookComponent },
   { path: 'edit/:id', component: EditBookComponent },
-  { path: '', redirectTo: '/books', pathMatch: 'full' }
+  
 ];
