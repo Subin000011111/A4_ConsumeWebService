f72f8e9b804d58ed8e52850f73ea2801b1c19954
Subin000011111 <subinsamuel104@gmail.com>
2025-04-14 23:37:55 -0400

    Implemented EditBookComponent with form to update existing book using ID

--- src/app/components/edit-book/edit-book.component.ts
+++ src/app/components/edit-book/edit-book.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BookService } from '../../services/book.service';
-import { Book } from '../../models/book.model';
+import { Book } from '../../models/book';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-book',
   standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './edit-book.component.html'
+  templateUrl: './edit-book.component.html',
+  styleUrls: ['./edit-book.component.css'],
+  imports: [CommonModule, FormsModule]
 })
-export class EditBookComponent {
-  book: Book = { id: 0, title: '', authorName: '', price: 0, quantity: 0 };
+export class EditBookComponent implements OnInit {
+  book: Book = {
+    id: 0,
+    title: '',
+    authorName: '',
+    price: 0,
+    quantity: 0
+  };
 
-  constructor(private route: ActivatedRoute, private bookService: BookService, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private bookService: BookService
+  ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -23,7 +34,7 @@ export class EditBookComponent {
     });
   }
 
-  onUpdate(): void {
+  onSubmit(): void {
     this.bookService.updateBook(this.book.id, this.book).subscribe(() => {
       this.router.navigate(['/books']);
     });
