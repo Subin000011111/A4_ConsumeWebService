7f401bbbc54a630b1e56d5b6d911775bb4232b10
Subin000011111 <subinsamuel104@gmail.com>
2025-04-14 23:43:36 -0400

    Updated AppComponent to standalone with router links and layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterModule],
+  template: `
+    <div class="container">
+      <h2>Consume Book WebServices</h2>
+      <ul>
+        <li><a routerLink="/books">Book List</a></li>
+        <li><a routerLink="/add">Add a Book</a></li>
+      </ul>
+      <router-outlet></router-outlet>
+      <br />
+      <p>Subin Samuel | 991753006</p>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'A4_ConsumeWebService';
-}
+export class AppComponent {}
