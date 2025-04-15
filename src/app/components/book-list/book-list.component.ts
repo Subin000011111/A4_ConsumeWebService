4ae216ed68b9d9d8df8c2484cfa77f43dddadf78
Subin000011111 <subinsamuel104@gmail.com>
2025-04-14 23:40:47 -0400

    Implemented Read and Delete functionality in BookListComponent

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookService } from '../../services/book.service';
-import { Book } from '../../models/book.model';
+import { Book } from '../../models/book';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './book-list.component.html'
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css'],
+  imports: [CommonModule]
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
   books: Book[] = [];
 
-  constructor(private bookService: BookService) {}
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks(): void {
     this.bookService.getBooks().subscribe(data => {
       this.books = data;
     });
+  }
+
+  deleteBook(id: number): void {
+    if (confirm('Are you sure you want to delete this book?')) {
+      this.bookService.deleteBook(id).subscribe(() => {
+        this.loadBooks(); // Refresh list
+      });
+    }
+  }
+
+  editBook(id: number): void {
+    this.router.navigate(['/edit', id]);
   }
 }
