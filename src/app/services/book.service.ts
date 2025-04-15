3c3288a60f6252484cf589143a71d37ea432d185
Subin000011111 <subinsamuel104@gmail.com>
2025-04-15 09:11:40 -0400

    Add getBookById() method in BookService to support EditBookComponent

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -3,34 +3,31 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Book } from '../models/book';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
-    private apiUrl = 'http://localhost:8080/api/books';
-
+  private baseUrl = 'http://localhost:8080/api/books';
 
   constructor(private http: HttpClient) {}
 
   getBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>(this.apiUrl);
+    return this.http.get<Book[]>(this.baseUrl);
   }
 
-  getBook(id: number): Observable<Book> {
-    return this.http.get<Book>(`${this.apiUrl}/${id}`);
+  getBookById(id: number): Observable<Book> {
+    return this.http.get<Book>(`${this.baseUrl}/${id}`);
   }
 
-  createBook(book: Book): Observable<Book> {
-    return this.http.post<Book>(this.apiUrl, book);
+  addBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.baseUrl, book);
   }
-  
 
   updateBook(id: number, book: Book): Observable<Book> {
-    return this.http.put<Book>(`${this.apiUrl}/${id}`, book);
+    return this.http.put<Book>(`${this.baseUrl}/${id}`, book);
   }
 
   deleteBook(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
   }
 }
