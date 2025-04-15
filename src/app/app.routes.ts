63013489c53bebecfdcf9af0e9adb696c5942b53
Subin000011111 <subinsamuel104@gmail.com>
2025-04-15 00:14:01 -0400

    Setup routing for Book List, Add Book, Edit Book; updated homepage to show links only

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,10 @@ import { Routes } from '@angular/router';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
 import { EditBookComponent } from './components/edit-book/edit-book.component';
-import { BookDetailsComponent } from './components/book-details/book-details.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: BookListComponent },  
   { path: 'books', component: BookListComponent },
-  { path: 'add-book', component: AddBookComponent },
+  { path: 'add', component: AddBookComponent },
   { path: 'edit/:id', component: EditBookComponent },
-  { path: 'details/:id', component: BookDetailsComponent }
+  { path: '', redirectTo: '/books', pathMatch: 'full' }
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { routes } from './app/app.routes';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule, FormsModule)
+    provideHttpClient(),
+    importProvidersFrom(FormsModule, HttpClientModule)
   ]
-});
+}).catch(err => console.error(err));
